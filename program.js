4bb423b14e55b37da4cdb950010ca198aefbc5cc
poetovio <urban.vizintin@outlook.com>
2022-10-20 01:33:09 +0200

    Display JSON datoteke

--- program.js
+++ program.js
@@ -2,10 +2,13 @@ const fs = require('fs');
 const converter = require('xml2js');
 
 const parser = new converter.Parser();
+let inspect = require('eyes').inspector({maxLength: false});
 
 fs.readFile(__dirname + '/xmlfile1.xml', (err, data) => {
     parser.parseString(data, (err, result) => {
-        console.dir(result);
+        inspect(result);
         console.log('done');
+        console.log(JSON.stringify(result));
     });
 });
+
