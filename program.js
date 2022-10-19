cca24b74ae974e3ba7b569cb3f220239950c2ccd
poetovio <urban.vizintin@outlook.com>
2022-10-20 01:47:34 +0200

    Shranjevanje datoteke v variable

--- program.js
+++ program.js
@@ -4,11 +4,8 @@ const converter = require('xml2js');
 const parser = new converter.Parser();
 let inspect = require('eyes').inspector({maxLength: false});
 
-fs.readFile(__dirname + '/xmlfile1.xml', (err, data) => {
-    parser.parseString(data, (err, result) => {
-        inspect(result);
-        console.log('done');
-        console.log(JSON.stringify(result));
-    });
-});
+let podjetje = fs.readFileSync(__dirname + '/xmlfile1.xml', 'utf-8');
 
+parser.parseString(podjetje, (err ,result) => {
+    inspect(result);
+});
