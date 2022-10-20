634c2a61ddf708eb89922f74ee60c69d259c034f
poetovio <urban.vizintin@outlook.com>
2022-10-20 12:00:01 +0200

    Skoraj kompletna koda

--- program.js
+++ program.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const converter = require('xml2js');
+const json2xml = require('json2xml');
 
 const parser = new converter.Parser();
 let inspect = require('eyes').inspector({maxLength: false});
@@ -26,10 +27,18 @@ urnikJSON.Urnik.Teden.filter((teden, i) => {
     }
 });
 
-console.log(filtriranUrnik);
+fs.writeFileSync('tedniPolni.json', JSON.stringify(filtriranUrnik), (err) => {
+    if(err) {
+        console.err(err);
+    }
+    console.log('Datoteka je bila uspesno ustvarjena.');
+})
 
 /*
-let pretvorba = builder.buildObject(filtriranUrnik);
-
-console.log(pretvorba);
+let pretvorba = builder.buildObje;ct(JSON.parse(JSON.stringify(filtriranUrnik)));
 */
+
+fs.readFile('tedniPolni.json', 'utf-8', (err, data) => {
+    if(err) { console.err(err); }
+    fs.writeFileSync('polniTedni.xml', json2xml(JSON.parse(data)));
+});
