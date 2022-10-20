39e6c031b84919f3dc333993e16ac43b6e2006a1
poetovio <urban.vizintin@outlook.com>
2022-10-20 02:32:19 +0200

    Updated XML file

--- program.js
+++ program.js
@@ -5,14 +5,27 @@ const parser = new converter.Parser();
 let inspect = require('eyes').inspector({maxLength: false});
 
 let podjetje = fs.readFileSync(__dirname + '/xmlfile1.xml', 'utf-8');
+let urnik = fs.readFileSync(__dirname + '/xmlfile2.xml', 'utf-8');
 
+let urnikJSON = null;
 let podjetjeJSON = null;
+
+parser.parseString(urnik, (err ,result) => {
+    urnikJSON = result;
+});
 
 parser.parseString(podjetje, (err ,result) => {
     podjetjeJSON = result;
 });
 
+let urnik2021 = urnikJSON.Urnik.Leto[0];
+let urnik2022 = urnikJSON.Urnik.Leto[1];
 
+
+
+console.log(urnik2022);
+
+/*
 let podjetjeNovo = JSON.parse(JSON.stringify(podjetjeJSON)).Podjetje;
 
 let osebje = podjetjeNovo.Osebje;
@@ -22,3 +35,4 @@ let zaposleni = osebje[0].Zaposleni;
 zaposleni[0].Zaposlen.filter((element) => {
     console.log(element.Ime + ' ' + element.Priimek);
 });
+*/
